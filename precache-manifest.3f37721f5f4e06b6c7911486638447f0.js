63815ee436d9b748c704431a6831cf9315a4c0f8
Max <office@bsmx.ru>
2020-07-12 15:12:21 +0300

    Updates

---
+++ precache-manifest.3f37721f5f4e06b6c7911486638447f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e97f2f3b578bb2933f907574a5b274",
+    "url": "/miniapp/index.html"
+  },
+  {
+    "revision": "8372df27183ddaef8c90",
+    "url": "/miniapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "24fff314903ac4024369",
+    "url": "/miniapp/static/js/2.5202703f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/miniapp/static/js/2.5202703f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8372df27183ddaef8c90",
+    "url": "/miniapp/static/js/main.4f8e9e02.chunk.js"
+  },
+  {
+    "revision": "1a059d5d65574a6dcf7c",
+    "url": "/miniapp/static/js/runtime-main.9beffdac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/miniapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
