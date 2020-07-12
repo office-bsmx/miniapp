94535189044421f1f7ffbd6b88dcea83f18388f2
Max <office@bsmx.ru>
2020-07-12 16:33:40 +0300

    Updates

---
+++ precache-manifest.5fe2ac5004a753f226f4b1581c34e3ff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a5f10ee9e77bb924b97da1c01573b57",
+    "url": "/miniapp/index.html"
+  },
+  {
+    "revision": "853ae598e5443673930b",
+    "url": "/miniapp/static/css/2.a6674286.chunk.css"
+  },
+  {
+    "revision": "61a8ccede4ed8deac02b",
+    "url": "/miniapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "853ae598e5443673930b",
+    "url": "/miniapp/static/js/2.be8aac05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/miniapp/static/js/2.be8aac05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61a8ccede4ed8deac02b",
+    "url": "/miniapp/static/js/main.c6570702.chunk.js"
+  },
+  {
+    "revision": "1a059d5d65574a6dcf7c",
+    "url": "/miniapp/static/js/runtime-main.9beffdac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/miniapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
